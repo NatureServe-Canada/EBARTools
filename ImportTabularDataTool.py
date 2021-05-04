c1b2cb72636981edf150c717fb0ba1fd6bb120e0
Randal Greene <rgreene@natureserve.ca>
2021-05-04 07:58:38 -0230

    Make SRS checking more generic on tabular import

--- ImportTabularDataTool.py
+++ ImportTabularDataTool.py
@@ -235,8 +235,12 @@ class ImportTabularDataTool:
             # assume WGS84 if not provided
             srs = EBARUtils.srs_dict['WGS84']
             if field_dict['srs']:
-                if file_line[field_dict['srs']].lower() not in ('unknown', 'not recorded',
-                                                                'not recorded (forced wgs84)'):
+                # srs provided
+                if file_line[field_dict['srs']].lower() not in ('unknown', 'not recorded'):
+                    if 'wgs84' in srs.lower():
+                        srs = 'WGS84'
+                    elif 'nad83' in srs.lower():
+                        srs = 'NAD83'
                     srs = EBARUtils.srs_dict[file_line[field_dict['srs']]]
             input_geometry = arcpy.PointGeometry(input_point, arcpy.SpatialReference(srs))
             output_geometry = input_geometry.projectAs(
